e6052bc7d675d49654a24a556dda538f57c969c6
Gustavo Tavares <ovatsug25@gmail.com>
2014-05-22 11:16:21 -0500

    add chsh to misc

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,5 @@
 alias reload!='. ~/.zshrc'
+alias cdd="cd ~/Desktop/Dropbox/"
+alias preview="open /Applications/Preview.app"
+alias safari="open /Applications/Safari.app"
+alias chrome="open /Applications/Chrome.app"
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -68,12 +68,12 @@ rb_prompt() {
 }
 
 directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+  echo "%{$fg_bold[blue]%}%1/%\/%{$reset_color%}"
 }
 
 export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
+  export RPROMPT="%{$fg_bold[blue]%}%{$reset_color%}"
 }
 
 precmd() {
