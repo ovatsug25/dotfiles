0503ee8a63d8210db2f9273e8a2fa02e7768e2b9
Gustavo <ovatsug25@gmail.com>
2014-05-22 14:19:24 -0500

    Add aliases to ZSH

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,13 @@
 alias reload!='. ~/.zshrc'
 alias cdd="cd ~/Desktop/Dropbox/"
 alias preview="open /Applications/Preview.app"
+
 alias safari="open /Applications/Safari.app"
 alias chrome="open /Applications/Chrome.app"
+
+
+alias js="open -a 'Google Chrome' http://0.0.0.0:4000/"
+alias jss="open http://0.0.0.0:4000/"
+
+alias rsg="open -a 'Google Chrome' http://0.0.0.0:4000/"
+alias rss="open http://0.0.0.0:4000/"
